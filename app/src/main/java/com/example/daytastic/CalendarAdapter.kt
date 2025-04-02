3abf4bbf7e24b8fa045497067ff1451f571aad30
Maor Shemesh <maorsh74@gmail.com>
2025-04-02 08:05:40 +0300

    Redesigned the calendar cell event.

--- app/src/main/java/com/example/daytastic/CalendarAdapter.kt
+++ app/src/main/java/com/example/daytastic/CalendarAdapter.kt
@@ -2,22 +2,23 @@ package com.example.daytastic
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.DrawableContainer.DrawableContainerState
 import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import android.util.Log
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import com.example.daytastic.ui.calender.CalendarCellModel
 import com.example.daytastic.ui.calender.CalendarEvent
 import com.example.daytastic.ui.calender.CalendarEventsInstance
-import com.example.daytastic.weather.WeatherInstance.weather
-import java.time.Duration
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -62,26 +63,30 @@ class CalendarAdapter(
             return
         }
         val eventListLL = holder.eventListLL
+        val typedValue = TypedValue()
         eventsListOfDate.sortBy { event -> event.startTime }
         eventsListOfDate.forEach{ event ->
-
             val eventItem = TextView(eventListLL.context).apply {
+                context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSecondaryContainer, typedValue,true)
+                setTextColor(typedValue.data)
+                setBackgroundResource(R.drawable.calendar_cell_event_item)
                 text = event.name
+                setPadding(8, 2, 2, 2)
                 textSize = TypedValue.applyDimension(
                     TypedValue.COMPLEX_UNIT_SP,
                     4F,
                     eventListLL.context.resources.displayMetrics
                 )
-                setBackgroundColor(event.color)
-                setOnClickListener { holder.onClick(eventListLL)
-                Log.d("event clicked", "Clicked on event, parent: $parent")}
+                setOnClickListener { holder.onClick(eventListLL) }
                 layoutParams = ViewGroup.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
                 )
             }
-                eventListLL.addView(eventItem)
-                Log.d("added event", "added event from parent: $eventListLL")
+            val drawable = eventItem.background as LayerDrawable
+            val innerBorder = drawable.findDrawableByLayerId(R.id.inner_border) as GradientDrawable
+            innerBorder.setStroke(5, event.color)
+            eventListLL.addView(eventItem)
         }
     }
 
